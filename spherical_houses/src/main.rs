3f0e536977664843229c8bbea6318cac237fa86d
Jeffrey Kim <kimjeffrey96@gmail.com>
2020-11-18 20:31:28 -0800

    Part 2

--- spherical_houses/src/main.rs
+++ spherical_houses/src/main.rs
@@ -2,68 +2,77 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Debug)]
-struct Santa {
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Point {
     x: isize,
     y: isize,
-    prev_locations: HashSet<(isize, isize)>, // vector of tuples (x, y)
 }
 
-impl Santa {
-    fn new(x: isize, y: isize) -> Santa {
-        let mut locations: HashSet<(isize, isize)> = HashSet::new();
-        locations.insert((x, y));
-
-        Santa {
+impl Point {
+    fn new(x: isize, y: isize) -> Point {
+        Point {
             x,
             y,
-            prev_locations: locations,
         }
     }
 
     // Directions
     fn go_north(&mut self) {
         self.y += 1;
-        self.update_direction();
     }
 
     fn go_south(&mut self) {
         self.y -= 1;
-        self.update_direction();
     }
 
     fn go_east(&mut self) {
         self.x += 1;
-        self.update_direction();
     }
 
     fn go_west(&mut self) {
         self.x -= 1;
-        self.update_direction();
     }
 
-    fn update_direction(&mut self) {
-        if let None = self.prev_locations.get(&(self.x, self.y)) {
-            self.prev_locations.insert((self.x, self.y));
+    fn get_point(&self) -> Point {
+        Point {
+            x: self.x,
+            y: self.y,
         }
     }
 }
 
 fn main() {
-    let mut santa: Santa = Santa::new(0, 0);
+    let mut set: HashSet<Point> = HashSet::new();
+    let mut santa: Point = Point::new(0, 0);
+    let mut robo_santa: Point = Point::new(0, 0);
+
+    let mut santa_turn: bool = true;
+    let mut robo_turn: bool = false;
+
+    set.insert(Point{x: 0, y:0});
+
+    // TODO: Change this file;
     let mut file: File = File::open("./house_course.txt").unwrap();
     let mut content: String = String::new();
 
     file.read_to_string(&mut content).unwrap();
 
     for c in content.chars() {
-        read_symbols(&mut santa, c);
+        if santa_turn {
+            read_symbols(&mut santa, c, &mut set);
+            santa_turn = false;
+            robo_turn = true;
+        } else if robo_turn {
+            read_symbols(&mut robo_santa, c, &mut set);
+            robo_turn = false;
+            santa_turn = true;
+        }
     }
 
-    println!("How many house? {}", santa.prev_locations.len());
+    println!("How many house? {}", set.len());
 }
 
-fn read_symbols(santa: &mut Santa, c: char) {
+fn read_symbols(santa: &mut Point, c: char, set: &mut HashSet<Point>) {
     match c {
         '^' => santa.go_north(),
         'v' => santa.go_south(),
@@ -73,12 +82,17 @@ fn read_symbols(santa: &mut Santa, c: char) {
             println!("Unknown symbol: {}", c);
         },
     }
+
+    println!("{:?}", santa.get_point());
+    set.insert(santa.get_point());
 }
 
+/*
 #[test]
 fn test_sample_one() {
-    let mut santa: Santa = Santa::new(0, 0);
-    let test_str = ">";
+    let mut santa: Point  = Santa::new(0, 0);
+    let mut robo_santa: Santa = Santa::new(0, 0);
+    let test_str = "^v";
 
     for c in test_str.chars() {
         read_symbols(&mut santa, c);
@@ -113,4 +127,4 @@ fn test_sample_three() {
     println!("{:?}", &santa.prev_locations);
     assert_eq!(santa.prev_locations.len(), 2);
 }
-
+*/
