ebadd7efd182a97915de7cc693ab2dde95703982
Jeffrey Kim <kimjeffrey96@gmail.com>
2020-12-25 00:28:48 -0800

    Add gitignore, moving on to part 2

--- 2020/report_repair/src/main.rs
+++ 2020/report_repair/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::collections::{HashMap, HashSet};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("list.txt").unwrap();
+    let mut reader = BufReader::new(file);
+
+    let mut num_list: Vec<i32> = Vec::new();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut set: HashSet<i32> = HashSet::new();
+
+    for line in reader.lines() {
+        let parse_int: i32 = line.unwrap().parse::<i32>().unwrap();
+        num_list.push(parse_int);
+    }
+
+    for (i, x) in num_list.iter().enumerate() {
+        for n in i + 1..=num_list.len() {
+            let compliment: i32 = 2020 - x - num_list.get(n).unwrap();
+            set.insert(compliment);
+        }
+    }
+
+    println!("{:?}", set);
 }
